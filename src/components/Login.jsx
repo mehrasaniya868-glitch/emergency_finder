2834e94acc8e0e8b7e0072925082d25d624b4705
saniya mehra <mehrasaniya868@gmail.com>
2026-04-26 20:16:21 +0530

    update name and phone input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,15 +2,32 @@ import React,{useState} from "react";
 import "./Login.css";
 const Login = ({onLogin}) => {
   const [countryCode , setCountryCode] = useState("+91");
-  const [error , setError] =useState("");
+  const [nameError , setNameError] =useState("");
+  const [phoneError, setPhoneError] = useState("");
   const [username, setUsername] = useState("");
   const [phone, setPhone] = useState("");
     const handleSubmit = () => {
-        if (!username || !phone) {
-            alert("Please enter both username and phone number.");
-            return;
+      let valid = true ;
+        if (!username) {
+            setNameError("Please enter your name");
+            valid = false;
           }
-          onLogin({username, phone});
+          else {
+            setNameError("");
+          }
+          if(!phone){
+            setPhoneError("Please enter phone number")
+            valid = false;
+          }
+          else if(phone.length !== 10){
+            setPhoneError("Phone must be 10 digits");
+            valid = false;
+          }
+          else{
+            setPhoneError("");
+          }
+       if(!valid)return;
+          onLogin({username, phone: countryCode + phone});
           localStorage.setItem("user", JSON.stringify({username, phone}));
     };
   return (
@@ -24,8 +41,10 @@ const Login = ({onLogin}) => {
         placeholder="Enter your name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+       
       />
-
+ {nameError && <p style={{ color: "red" }}>{nameError}</p>}
+        
       <input
      
         type="text"
@@ -37,19 +56,18 @@ const Login = ({onLogin}) => {
             if(value.length<=10){
               setPhone(value);
             }
+          
           }}
         
       />
-
+      {phoneError && <p style={{ color: "red" }}>{phoneError}</p>}
+       
       <button onClick={handleSubmit}>
         Continue
       </button>
-     
-      
     </div>
   </div>
 );
-
 };
 export default Login;
   
